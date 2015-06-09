02b7c1fbb87538e78aa5ee64208f424044304084
Jake Pelletier <jake.pelletier@sendgrid.com>
2015-06-09 16:23:59 -0600

    More early stuff

--- hive.go
+++ hive.go
@@ -1,7 +1,25 @@
-package main
+// A service that manages interactions between other services
+package hive
 
-import "fmt"
+import (
+  "fmt"
+
+  "github.com/yosida95/golang-jenkins"
+)
 
 func main() {
   fmt.Printf("Hello, world.\n")
+  auth := &gojenkins.Auth{"Ghjnut", "Token123"}
+  jenkins := gojenkins.NewJenkins(auth, "http://127.0.0.1")
+  jobs, err := jenkins.GetJobs()
+
+  fmt.Printf("%+v", jobs)
+
+  if err != nil {
+    fmt.Printf("error %v\n", err)
+  }
+
+  if len(jobs) == 1 {
+    fmt.Printf("return no jobs\n")
+  }
 }
